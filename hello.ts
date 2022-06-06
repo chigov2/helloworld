f6a29de45b688708d44d06cac7a4641bd3e72fef
Max2 <chigov@gmail.com>
2022-06-06 16:26:09 +0300

    Initial commit

---
+++ hello.ts
@@ -0,0 +1,11 @@
+let found: boolean = true;
+let grade: number = 88;
+let firstName: string = "Mike";
+let lastName: string = "Chigov";
+
+
+
+console.log(found);
+console.log("The Age is: " + grade);
+console.log("Hi, my name is " + firstName + " " + lastName);
+console.log(`Hi, ${firstName} ${lastName}`);
